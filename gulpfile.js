5aaf1358320a032001f810955e2ae63f9ab6cabb
twem007 <yu.mountains@gmail.com>
2017-09-18 18:41:44 +0800

    添加项目自动编译

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp');
 var del = require('del');
 var ts = require('gulp-typescript');
-var tsProject = ts.createProject('tsconfig.json');
+var sourcemaps = require('gulp-sourcemaps');
+var tsProject = ts.createProject('./code/client/tsconfig.json');
 
 gulp.task('clean', function (cb) {
   console.log("执行清理流程");
@@ -10,18 +11,17 @@ gulp.task('clean', function (cb) {
   });
 });
 
-// 返回一个 callback，因此系统可以知道它什么时候完成
-gulp.task('build', ['clean'], function (cb) {
-  gulp.src("./code/client/src/**/*.ts")
-    .pipe(tsProject())
-    .pipe(gulp.dest('./code/client/bin-debug'))
-    .on('end', cb);
+gulp.task('compile', function () {
+    return tsProject.src()
+        .pipe(sourcemaps.init())
+        .pipe(tsProject())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('./code/client/bin-debug'))
+        .on('end', cb);
 });
 
-// 定义一个所依赖的 task 必须在这个 task 执行之前完成
-gulp.task('test', ['clean', 'build'], function () {
-  // 'build' 完成后
+gulp.task('test', ['clean', 'compile'], function () {
   console.log("执行测试流程");
 });
 
-gulp.task('default', ['clean', 'build', 'test']);
+gulp.task('default', ['clean', 'compile', 'test']);
