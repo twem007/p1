73caf1fb98fe8a60b5ebe5a07827d5849dff907e
twem007 <yu.mountains@gmail.com>
2017-10-27 10:19:01 +0800

    1、为事件模块添加耗时统计，方便性能调优 2、为DebugUtils添加日志开关

--- code/client/src/core/utils/DebugUtils.ts
+++ code/client/src/core/utils/DebugUtils.ts
@@ -7,7 +7,14 @@ module core {
 	export class DebugUtils {
 
 		private static s_dic: Dictionary<number> = new Dictionary<number>();
-
+		/**
+		 * 系统事件日志开关
+		 */
+		public static EVENT_LOG:boolean = true;
+		/**
+		 * 系统事件日志统计边界
+		 */
+		public static EVENT_LIMIT:number = 5;
 		/**
 		 * 开始时间戳
 		 */
