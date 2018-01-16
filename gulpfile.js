a17890b46e0449f95ae88b8536c52722d555dc6d
twem007 <yu.mountains@gmail.com>
2018-01-16 10:19:19 +0800

    1、修改自动测试流程

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
 var del = require('del');
 var ts = require('gulp-typescript');
-var sourcemaps = require('gulp-sourcemaps');
 var tsProject = ts.createProject('./code/client/tsconfig.json');
 
 gulp.task('clean', function (cb) {
@@ -9,17 +8,11 @@ gulp.task('clean', function (cb) {
   del(['./code/client/bin-debug/**/*'], cb);
 });
 
-gulp.task('compile', function () {
+gulp.task('compile', ['clean'], function () {
   console.log("开始编译");
   return tsProject.src()
-    .pipe(sourcemaps.init())
     .pipe(tsProject())
-    .pipe(sourcemaps.write())
     .pipe(gulp.dest('./code/client/bin-debug'))
 });
 
-gulp.task('test', ['clean', 'compile'], function () {
-  console.log("开始测试");
-});
-
-gulp.task('default', ['clean', 'compile', 'test']);
+gulp.task('default', ['clean', 'compile']);
