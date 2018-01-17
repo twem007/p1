7261f2b4dfb11c82f33c3de3954165588e54e937
twem007 <yu.mountains@gmail.com>
2018-01-17 13:50:30 +0800

    1、自动编译测试

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,15 @@ var ts = require('gulp-typescript');
 var tsProject = ts.createProject('./code/client/tsconfig.json');
 
 gulp.task('clean', function (cb) {
-  console.log("开始清理");
   return del(['./code/client/bin-debug/**/*']);
 });
 
 gulp.task('compile', ['clean'], function () {
-  console.log("开始编译");
   return tsProject.src()
     .pipe(tsProject())
     .pipe(gulp.dest('./code/client/bin-debug'))
 });
 
-gulp.task('default', ['clean', 'compile']);
+gulp.task('default', ['clean', 'compile'], function () {
+  console.log("编译测试完成");
+});
