94f44bfd970bcece6a29d9d4d56ebc11b4c50b7d
twem007 <yu.mountains@gmail.com>
2018-02-05 10:53:28 +0800

    1、添加LocalStorage接口注释

--- code/client/src/core/data/LocalStorageData.ts
+++ code/client/src/core/data/LocalStorageData.ts
@@ -2,11 +2,17 @@ module core {
     export class LocalStorageData {
 
         private static hashID: string;
-
-        public static setHashID(openid: string): void {
-            LocalStorageData.hashID = openid;
+        /**
+         * @param hashID 设置数据唯一ID，用于数据隔离
+         */
+        public static setHashID(hashID: string): void {
+            LocalStorageData.hashID = hashID;
         }
-
+        /**
+         * @param key       数据KEY
+         * @param value     数据值
+         * @param isGlobal  是否全局数据(全局数据KEY唯一，非全局数据每个hashID分别保存KEY)
+         */
         public static setItem(key: string, value: string, isGlobal: boolean = false): void {
             if (isGlobal) {
                 egret.localStorage.setItem(key, value);
@@ -14,7 +20,11 @@ module core {
                 egret.localStorage.setItem(`${LocalStorageData.hashID}_${key}`, value);
             }
         }
-
+        /**
+         * 获取数据
+         * @param key       数据KEY
+         * @param isGlobal  是否全局数据(全局数据KEY唯一，非全局数据每个hashID分别保存KEY)
+         */
         public static getItem(key: string, isGlobal: boolean = false): string {
             if (isGlobal) {
                 return egret.localStorage.getItem(key);
@@ -22,7 +32,11 @@ module core {
                 return egret.localStorage.getItem(`${LocalStorageData.hashID}_${key}`);
             }
         }
-
+        /**
+         * 删除数据
+         * @param key       数据KEY
+         * @param isGlobal  是否全局数据(全局数据KEY唯一，非全局数据每个hashID分别保存KEY)
+         */
         public static removeItem(key: string, isGlobal: boolean = false): void {
             if (isGlobal) {
                 egret.localStorage.removeItem(key);
@@ -30,7 +44,9 @@ module core {
                 egret.localStorage.removeItem(`${LocalStorageData.hashID}_${key}`);
             }
         }
-
+        /**
+         * 清空数据
+         */
         public static clearAll(): void {
             egret.localStorage.clear();
         }
